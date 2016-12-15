8a56c8f9f9f63dbf4707ba6a4a97150c4c7d3b81
Ali Nouri <ali.nouri@gmail.com>
2016-12-14 20:13:25 -0800

    update arduino code to work with RF24
    This CL still has some temp code because we don't have RF24 circuit for
    the other foot to test.
    Search for TODO to find the code.

--- arduino-razor/razor/razor/razor.ino
+++ arduino-razor/razor/razor/razor.ino
@@ -5,10 +5,13 @@
 
 // So we can communicate to the Bluetooth module.
 // Make sure you put a symlink to the IMUProcessor.h in the Arduino libraries for this to work.
+#include <SPI.h>
+
 #include "IMUProcessor.h"
-#include "sensor.pb.h"
+#include "RF24.h"
 #include "pb_encode.h"
-#include "vcc.h"
+#include "rf24utils.h"
+#include "sensor.pb.h"
 
 // Uncomment one of these lines based on what board you want to use the code with:
 //  #define BOARD_UNO
@@ -29,40 +32,97 @@ const int kInterruptPin = 12;
 const int kPressureFrontPin = A3;
 // Interrupt pin used for MPU-9250.
 const int kInterruptPin = 7;
-#endif
-#endif
 
+// CE and CSN ports for the RF24 transmitter.
+const int kRF24CE = 0;
+const int kRF24CSN = 1;
+
+// Connect this pin to ground for the slave one, leave it open for master.
+const int kSlaveDetectorPin = 5;
+#endif
+#endif
 
 IMUProcessor imu_processor(kInterruptPin, false /* no debug */, true /* get all data */);
+
+// Where we store the sensor data.
 SensorData sensor_data = SensorData_init_zero;
+FootSensorData* foot_data = 0;  // This will point to the foot we're running on.
+
+RF24 rf_radio(kRF24CE, kRF24CSN);
+RFIO rfio(&rf_radio);
+byte pipe_address[][6] = {"1Node"};
+
+// How often the two feet communicate with each other.
+const int kBetweenFeetDataSyncMsec = 100;
+// How often master foot communicates with phone.
+const int kPhoneDataSyncMsec = 500;
+
+// Which foot are we running as.
+// Left foot is the slave. It only reads sensor data and RF sends it to right foot.
+// Right foot is the master. Receives data from left foot, packages it into the
+// proto and bluetooth sends it to the cell phone.
+//
+// We automatically detect this by connecting kSlaveDetectorPin to ground for the
+// slave circuit, while leaving it open for the master.
+bool is_right_foot;
+
+void setupRF24() {
+  rf_radio.begin();
+  rf_radio.setChannel(108);  // For our purpose anything works. This is above wifi.
+  rf_radio.setPALevel(RF24_PA_MIN);
+
+  if (is_right_foot) {
+    rf_radio.openReadingPipe(1, pipe_address[0]);
+    rf_radio.startListening();
+  } else {
+    rf_radio.openWritingPipe(pipe_address[0]);
+  }
+}
 
 void setup() {
+  pinMode(5, INPUT_PULLUP);
+
   // To write any messages to the serial for debugging etc.
   Serial.begin(38400);
 
+  // This is just to make sur our println statements work in setup function.
+  delay(3000);
   Serial.println(F("Welcome to Razor!"));
 
-  // Set the data rate for the SoftwareSerial port. Bluetooth module works on 9600, so
-  // don't change this.
-  Serial1.begin(9600);
+  is_right_foot = digitalRead(kSlaveDetectorPin) == HIGH;
 
-  imu_processor.init();
+  // TODO: fix the temp code.
+  if (is_right_foot) {
+    imu_processor.init();
+  }
+
+  setupRF24();
+
+  if (is_right_foot) {
+    Serial.println(F("Running as master for right foot"));
+    // Set the data rate for the SoftwareSerial port. Bluetooth module works on 9600, so
+    // don't change this.
+    Serial1.begin(9600);
+  
+    foot_data = &(sensor_data.right);
+  } else {
+    Serial.println(F("Running as slave for left foot"));
+    foot_data = &(sensor_data.left);
+  }
 }
 
 unsigned long last_time = millis();
 
-void loop() {
+void loopForMaster() {
   uint8_t buffer[160];
-
-  // Always read the data, so the data can be calculated correctly.
-  imu_processor.readData(&(sensor_data.left.imu_data));
-
-  // Print the data only every 2s.
-  if (millis() - last_time > 500) {
-    // Read Analog sensor data:
-    sensor_data.left.pressure_front = analogRead(kPressureFrontPin);
-
-    // Things we need to send protos over bluetooth.
+  // Receive data from the other foot if available.
+//    rf_radio.read(&(sensor_data.left), sizeof(FootSensorData));
+  if (rfio.readFootSensorData(&(sensor_data.left))) {
+    Serial.println(F("Received slave foot data"));
+  }
+  
+  if (millis() - last_time > kPhoneDataSyncMsec) {
+    // Things we need to send over bluetooth.
     pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
     bool status = pb_encode_delimited(&stream, SensorData_fields, &sensor_data);
     if (!status) {
@@ -70,8 +130,43 @@ void loop() {
     } else {
 //      Serial.print(F("Wrote bytes: ")); Serial.println(stream.bytes_written);
       Serial1.write(buffer, stream.bytes_written);
+      sensor_data = SensorData_init_zero;
     }
     last_time = millis();
+  }
+}
+
+void loopForSlave() {
+  if (millis() - last_time > kBetweenFeetDataSyncMsec) {
+    rfio.writeFootSensorData(*foot_data);
+    Serial.println(rf_radio.getPayloadSize());
+    last_time = millis();
+  }
+}
+
+void loop() {
+  // Always read the data, so the data can be calculated correctly.
+  // TODO: get rid of the temp code here.
+  if (is_right_foot) {
+    imu_processor.readData(&(foot_data->imu_data));
+  } else {
+    foot_data->imu_data.ax = 11;
+    foot_data->imu_data.ay = 12;
+    foot_data->imu_data.az = 13;
+    foot_data->imu_data.gx = 14;
+    foot_data->imu_data.gy = 15;
+    foot_data->imu_data.gz =16;
+    foot_data->imu_data.yaw = 17;
+    foot_data->imu_data.pitch = 18;
+    foot_data->imu_data.roll = 19;
+    foot_data->imu_data.temperature = 20;
+    foot_data->pressure_front = 21; // analogRead(kPressureFrontPin);
+  }
+  
+  if (is_right_foot) {
+    loopForMaster();
+  } else {
+    loopForSlave();
   }
 }
 
---
+++ arduino-razor/razor/razor/rfio.h
@@ -0,0 +1,58 @@
+#ifndef RF_IO_H
+#define RF_IO_H
+
+#define PACKET_SIZE 32
+
+// A class that makes it easier to send struct data over RF24.
+// If the struct is less than PACKET_SIZE, which is 32 bytes you don't need to use
+// this because you can send everything in one call. If it is over that, we need
+// to break the struct into smaller pieces.
+class RFIO {
+  public:
+    RFIO(RF24* rf_radio)
+        : rf_radio_(rf_radio), reading_index_(0) {}
+
+    // Tries to make a reading of one packet from RF24 if data is available.
+    // If it is available and the current received packet completes the
+    // struct, updates foot_data and returns true.
+    // Otherwise, this will be a partial reading of the struct and will return false.
+    bool tryReadFootSensorData(FootSensorData* foot_data) {
+      if (!rf_radio_->available()) {
+        return false;
+      }
+
+      // How many bytes are remaining from the struct.
+      int remaining = sizeof(FootSensorData) - reading_index_;
+      // How many bytes should we read this time?
+      int this_read = remaining > PACKET_SIZE ? PACKET_SIZE : remaining;
+
+      rf_radio_->read(&(buffer_[reading_index_]), this_read);
+
+      // This read finished our struct?
+      if (this_read == remaining) {
+        reading_index_ = 0;
+        memcpy(foot_data, buffer_, sizeof(FootSensorData));
+        return true;
+      } else {
+        reading_index_ += this_read;
+        return false;
+      }
+    }
+
+    // Writes foot_data over RF24, possibly using multiple packets.
+    void writeFootSensorData(const FootSensorData& foot_data) {
+      byte* buffer = (byte*)(&foot_data);
+      for (int i = 0; i < sizeof(FootSensorData); i+= PACKET_SIZE) {
+        int this_write = sizeof(FootSensorData) - i > PACKET_SIZE ? PACKET_SIZE : sizeof(FootSensorData) - i;
+        rf_radio_->write(&(buffer[i]), this_write);
+      }
+    }
+
+  private:
+    RF24* rf_radio_;
+    byte buffer_[sizeof(FootSensorData)];
+    int reading_index_;
+};
+
+#endif  // RF_IO_H
+
